90b068a5b35d40374604be65830ef17c733c99e4
Dariusz Lacheta <lacheta.dariusz@gmail.com>
2018-12-14 11:57:21 +0200

    Moving scss styled to styled-components

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styled from 'styled-components';
 import posed, { PoseGroup } from 'react-pose';
-import './styles/style.scss';
 import Home from './components/Home';
 import Article from './components/Article';
+
+const Container = styled.div`
+  margin: 0;
+  padding: 20px;
+  color: black;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  background: #fff;
+
+  & a {
+    color: #000;
+  }
+`;
 
 const RouteContainer = posed.div({});
 
 const App = () => (
-  <Route
-    render={({ location }) => (
-      <PoseGroup>
-        <RouteContainer key={location.pathname}>
-          <Switch location={location}>
-            <Route exact path="/" component={Home} key="home" />
-            <Route path="/article" component={Article} key="article" />
-          </Switch>
-        </RouteContainer>
-      </PoseGroup>
-    )}
-  />
+  <Container>
+    <Route
+      render={({ location }) => (
+        <PoseGroup>
+          <RouteContainer key={location.pathname}>
+            <Switch location={location}>
+              <Route exact path="/" component={Home} key="home" />
+              <Route path="/article" component={Article} key="article" />
+            </Switch>
+          </RouteContainer>
+        </PoseGroup>
+      )}
+    />
+  </Container>
 );
 ReactDOM.render(
   <BrowserRouter>
